532a89a8f1c9b07fc3fdae837c51ec6f41f328a2
Shaggy <shaggybelco1@gmail.com>
2023-02-12 22:38:57 +0200

    all

--- Blank/src/environments/environment.prod.ts
+++ Blank/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:3333/api',
+  baseUrl: 'https://metto-chatting.onrender.com/api',
+  baseUrls: 'http://localhost:3333/api',
   cloud_name: 'dqgb5h17l',
   api_key: '281331871495664',
   api_secret: '_nNPAd9aIwMzX2kREA0NEb_Utsg',
--- Blank/src/environments/environment.ts
+++ Blank/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:3333/api',
+  baseUrl: 'https://metto-chatting.onrender.com/api',
+  baseUrls: 'http://localhost:3333/api',
   cloud_name: 'dqgb5h17l',
   api_key: '281331871495664',
   api_secret: '_nNPAd9aIwMzX2kREA0NEb_Utsg',
