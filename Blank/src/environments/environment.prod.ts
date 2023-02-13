00fbbff09a60d9ca054824e0802f9c317657ef35
Shaggy <shaggybelco1@gmail.com>
2023-02-13 05:41:23 +0200

    all

--- Blank/src/environments/environment.prod.ts
+++ Blank/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'metto-chatting-app-backend-production.up.railway.app',
+  baseUrl: 'https://metto-chatting-app-backend-production.up.railway.app',
   baseUrlr: 'https://metto-chatting.onrender.com/api',
   baseUrls: 'http://localhost:3333/api',
   cloud_name: 'dqgb5h17l',
--- Blank/src/environments/environment.ts
+++ Blank/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  baseUrl: 'metto-chatting-app-backend-production.up.railway.app/api',
+  baseUrl: 'https://metto-chatting-app-backend-production.up.railway.app/api',
   baseUrlr: 'https://metto-chatting.onrender.com/api',
   baseUrls: 'http://localhost:3333/api',
   cloud_name: 'dqgb5h17l',
