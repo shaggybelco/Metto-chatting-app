595aab3f9fdbb7d7789c5bc3e7c6fc6c7008ce53
shaggy <shaggy.sambo@dadevs.co.za>
2023-02-15 11:23:00 +0200

    all

--- Backend/App/Controllers/getUsers.controller.js
+++ Backend/App/Controllers/getUsers.controller.js
@@ -7,7 +7,7 @@ const GroupMember = db.groupmembers;
 module.exports.getusers = async (req, res) => {
   try {
     const users = await User.find({});
-
+console.log(users);
     res.status(200).json(users);
   } catch (error) {
     console.log(error.message);
