a00708717877e4eace410a3bdcd2cb9e0d7f688a
shaggy <shaggy.sambo@dadevs.co.za>
2023-02-15 12:36:16 +0200

    all

--- Blank/src/app/home/home.page.ts
+++ Blank/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StatusBar } from '@capacitor/status-bar';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,8 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() {
+    StatusBar.setBackgroundColor({color: '#3880ff'});
+  }
 
 }
