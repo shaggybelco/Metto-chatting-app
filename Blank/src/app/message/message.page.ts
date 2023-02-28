19436936c00017573630a4711d779fd7440d306b
shaggy <shaggy.sambo@dadevs.co.za>
2023-02-28 16:00:59 +0200

    all

--- Blank/src/app/message/message.page.ts
+++ Blank/src/app/message/message.page.ts
@@ -149,12 +149,15 @@ export class MessagePage implements OnInit {
   }
 
   page = 1;
+  isInfiniteScroll = false;
 
   onIonInfinite(ev: any) {
+    this.isInfiniteScroll = true;
     this.page++;
     this.getMessages();
     setTimeout(() => {
       (ev as InfiniteScrollCustomEvent).target.complete();
+      this.isInfiniteScroll = false;
     }, 500);
   }
 
