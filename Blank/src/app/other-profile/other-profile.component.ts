87883050e5bb3b055e610a8e8915168df8229916
Shaggy <shaggybelco1@gmail.com>
2023-03-02 19:17:46 +0200

    profile touch up

--- Blank/src/app/other-profile/other-profile.component.ts
+++ Blank/src/app/other-profile/other-profile.component.ts
@@ -1,6 +1,6 @@
 import { UserService } from '../services/user.service';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { StatusBar } from '@capacitor/status-bar';
 import { TokenService } from '../services/token.service';
 
@@ -10,7 +10,7 @@ import { TokenService } from '../services/token.service';
   styleUrls: ['./other-profile.component.scss'],
 })
 export class OtherProfileComponent implements OnInit {
-  constructor(public prof: UserService, private route: ActivatedRoute,private token: TokenService) {
+  constructor(public prof: UserService, private route: ActivatedRoute, private token: TokenService, private router: Router) {
     StatusBar.setBackgroundColor({ color: '#3dc2ff' });
   }
   me: any = {
@@ -31,31 +31,35 @@ export class OtherProfileComponent implements OnInit {
   }
 
   getMe() {
-    if (this.type === 'user') {
-      this.prof.getMe(this.otherId).subscribe({
-        next: (res: any) => {
-          console.log(res[0]);
-          this.me = res[0];
-          this.username = this.me.name;
-        },
-        error: (err: any) => {
-          console.log(err);
-        },
-      });
-    } else if (this.type === 'group') {
-      this.prof.getGroup(this.otherId).subscribe({
-        next: (res: any) => {
-          console.log(res);
-          this.me = res.group;
-          this.member = res.members;
-          this.username = this.me.name;
-          this.created_by = res.group.created_by._id
-          console.log(this.created_by);
-        },
-        error: (err: any) => {
-          console.log(err);
-        },
-      });
+    if (this.otherId === this.hold.id) { 
+      this.router.navigate(['/tab/profile'])
     }
+    else
+      if (this.type === 'user') {
+        this.prof.getMe(this.otherId).subscribe({
+          next: (res: any) => {
+            console.log(res[0]);
+            this.me = res[0];
+            this.username = this.me.name;
+          },
+          error: (err: any) => {
+            console.log(err);
+          },
+        });
+      } else if (this.type === 'group') {
+        this.prof.getGroup(this.otherId).subscribe({
+          next: (res: any) => {
+            console.log(res);
+            this.me = res.group;
+            this.member = res.members;
+            this.username = this.me.name;
+            this.created_by = res.group.created_by._id
+            console.log(this.created_by);
+          },
+          error: (err: any) => {
+            console.log(err);
+          },
+        });
+      }
   }
 }
